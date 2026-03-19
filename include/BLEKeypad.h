87dcd1d7882ebacb6470f86544db801178ae8c0d
Thomas Seren <thomas.seren@mailbox.org>
2026-03-19 08:26:31 +0100

    preparation to move code to other files for better readability

---
+++ include/BLEKeypad.h
@@ -0,0 +1,30 @@
+#ifndef BLEKEYPAD_H
+#define BLEKEYPAD_H
+
+#define Vol_p ((char) '+')
+#define Vol_m ((char) '-')
+#define ESC ((char) 'e')
+#define Home ((char) 'h')
+#define Power ((char) 'o')
+#define Backspace ((char) 'b')
+#define Enter ((char) 'n')
+#define ND ((char) ' ')
+#define Tab ((char) 's')
+#define BL ((char) 'l')
+
+#define XCT ((char) 'x')
+#define REC ((char) 'r')
+#define AltTab ((char) 's')
+
+
+const String key_chars =  String(ESC) + String(Backspace) + String(Power) + String(Enter) + String(Tab) ;
+const uint8_t key_array[] = {KEY_ESCAPE,   KEY_BACKSPACE,      KEY_POWER,      KEY_RETURN,     KEY_TAB};
+const String media_chars =     String(Vol_p) +  String(Vol_m) +    String(Home) +      String(BL);
+const uint16_t media_array[] = {MEDIA_VOLUME_UP, MEDIA_VOLUME_DOWN, MEDIA_BROWSER_HOME, MEDIA_DISPLAY_BACKLIGHT};
+const String no_repeat = String(Power) + String(AltTab) + String(Enter)+ String(Home) + String(REC) + String(ESC) + String(XCT);
+
+
+void myFunc1(int x);
+char myFunc2(char arr[], int len);
+
+#endif
---
+++ src/BLEKeypad.cpp
@@ -0,0 +1,5 @@
+#include <Arduino.h> 
+#include <HijelHID_BLEKeyboard.h>
+#include <Keypad.h>
+
+#include "BLEKeypad.h"
